e63768a23d8a76a63bea4463eebd148c020fc1f8
teharlevo <teharlevo@gmail.com>
2023-10-24 01:45:45 +0300

    רק בדיקה לגבי הרנצים גיט האב

--- src/scenes/TestScene.java
+++ src/scenes/TestScene.java
@@ -124,7 +124,7 @@ public class TestScene extends Scene{
             tm.cangeText("lol");
         }
         else{
-            tm.cangeText("time" + Window.time());
+            tm.cangeText("time" + Window.time());//lol
         }
     }
 }
